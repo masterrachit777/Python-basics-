7dc12aa42d737d9ba101608b66a31c7c878e410b
masterrachit777 <56754641+masterrachit777@users.noreply.github.com>
2019-12-14 14:27:36 +0530

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,6 @@
-'''age=int(input("Enter your age: "))
+age=int(input("Enter your age: "))
 if age>=18:
-    print("You are an adult")
+    print("You are an adult")   #no brackets are used in python, indentation does the work.
 else:
     print("You are not an adult")
 
@@ -15,7 +15,14 @@ elif day=="Tuesday":
     print("Hanuman chalisa")
 else:
     print("Party")
-print("End")'''
+print("End")
+
+#use hashtag to input a single line comment
+'''Use these triple inverted commas to insert
+comments in two or more then two lines.
+At the end of you multiple line comment again put
+these 3 commas to mark the end. it is not needed 
+in case of a single line comment'''
 
 a=3
 b=5
