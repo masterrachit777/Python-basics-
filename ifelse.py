86f8c97b469725bc051a1b589e9f6ef6b6d5ad56
masterrachit777 <56754641+masterrachit777@users.noreply.github.com>
2019-12-14 14:23:03 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,33 @@
+'''age=int(input("Enter your age: "))
+if age>=18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
+
+print("end of program")
+
+day = input("enter the day: ")
+if day=="Sunday":
+    print("woohoo")
+elif day=="Monday":
+    print("Go to work")
+elif day=="Tuesday":
+    print("Hanuman chalisa")
+else:
+    print("Party")
+print("End")'''
+
+a=3
+b=5
+c=7
+if a > b:
+    if a > c:
+        print(a, 'is largest')
+    else:
+        print(c, 'is largest')
+else:
+    if b > c:
+        print(b, 'is largest')
+    else:
+        print(c, 'is largest')
+
